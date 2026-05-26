5abfad204f83b6878ccc7bc3d5981269efe752ab
JoaoAichinger <aichinger.joao@gmail.com>
2026-05-25 22:22:22 -0300

    Select relevant features based on model coefficients. Change titanic DataSet

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 def preprocess(df):
     
     #Remove Useless Columns
-    df = df.drop(columns='Cabin')
+    df = df.drop(columns=['Cabin', 'Fare', 'Embarked'])
 
     #Fill Columns with Median Values for consistency
     m = df['Age'].median()
@@ -13,19 +13,15 @@ def preprocess(df):
     #Encode 'Sex' Column
     df['Sex'] = df['Sex'].replace(['male', 'female'], [0,1]) 
 
-    #Normalize "Embarked" Column
-    df = pd.concat([df, pd.get_dummies(df['Embarked'], prefix='Embarked')], axis=1)
-    df = df.drop(columns=['Embarked'])
-
     #Scale Columns to avoid magnitude priorization
     scaler = StandardScaler()
     df['Age'] = scaler.fit_transform(df[['Age']])
-    df['Fare'] = scaler.fit_transform(df[['Fare']])
 
     #Create Features
-    df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
     df['isAlone'] = False
-    df.loc[df['FamilySize'] == 1, 'isAlone'] = True
+    df.loc[(df['SibSp'] + df['Parch'])== 0, 'isAlone'] = True
+
+    
 
     #Drop columns processed for features
     df = df.drop(columns=['SibSp', 'Parch'])
